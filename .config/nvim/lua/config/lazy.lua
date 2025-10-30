2f361103e480c21274b172f5ab7caa0a667cfb38
Matthew Thiessen <matt.thiessen.dev@gmail.com>
2025-10-30 15:18:15 -0700

    feat: use case insensitive & smartsearch in vim (#1)

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -22,6 +22,9 @@ vim.opt.guicursor = ""
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
